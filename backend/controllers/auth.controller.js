185393bdcd70452c9e1850970e1afe6564a71efd
Zinger Marat <zingermarat@gmail.com>
2025-09-18 09:29:14 +0300

    Implement user authentication with signup and login routes; add auth controller for handling user registration and JWT token generation.

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,59 @@
+import User from "../models/user.model.js"
+import bcryptjs from "bcryptjs"
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+export const login = (req, res) => {}
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body
+
+  try {
+    if (!username || !email || !password) {
+      throw new Error("All fields are required")
+    }
+
+    const emailExists = await User.findOne({ email })
+    const userNameExists = await User.findOne({ username })
+
+    if (emailExists) {
+      return res.status(400).json({ message: "User already exists." })
+    }
+    if (userNameExists) {
+      return res.status(400).json({ message: "Username is taken, try another name" })
+    }
+
+    //hashed password
+    const hashedPassword = await bcryptjs.hash(password, 10)
+
+    const newUser = await User.create({
+      username: username,
+      email: email,
+      password: hashedPassword,
+    })
+
+    //JWT
+    if (!newUser) {
+      return res.status(500).json({ message: "Failed to create user" })
+    }
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, { expiresIn: "7d" })
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      signed: true,
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: process.env.NODE_ENV === "production" ? true : false,
+    })
+
+    res.status(201).json({
+      username: newUser.username,
+      email: newUser.email,
+      message: "User created successfully",
+    })
+  } catch (err) {
+    res.status(500).json({ message: err.message || "Server error" })
+  }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,7 @@
-import express, { json } from "express"
+import express from "express"
 import { connectToDB } from "./config/db.js"
 import dotenv from "dotenv"
-import User from "./models/user.model.js"
-import bcryptjs from "bcryptjs"
-import jwt from "jsonwebtoken"
+import authRouter from "./routes/auth.router.js"
 
 dotenv.config()
 
@@ -16,50 +14,7 @@ app.get("/", (req, res) => {
   res.send("hi ;)")
 })
 
-app.post("/api/signup", async (req, res) => {
-  const { username, email, password } = req.body
-
-  try {
-    if (!username || !email || !password) {
-      throw new Error("All fields are required")
-    }
-
-    const emailExists = await User.findOne({ email })
-    const userNameExists = await User.findOne({ username })
-
-    if (emailExists) {
-      return res.status(400).json({ message: "User already exists." })
-    }
-    if (userNameExists) {
-      return res.status(400).json({ message: "Username is taken, try another name" })
-    }
-
-    //hashed password
-    const hashedPassword = await bcryptjs.hash(password, 10)
-
-    const newUser = await User.create({
-      username: username,
-      email: email,
-      password: hashedPassword,
-    })
-
-    //JWT
-    if (!newUser) {
-      return res.status(500).json({ message: "Failed to create user" })
-    }
-
-    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, { expiresIn: "7d" })
-
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-    })
-    res.status(201).json({ email: newUser.email, message: "User created successfully" })
-  } catch (err) {
-    res.status(500).json({ message: err.message || "Server error" })
-  }
-})
+app.use("/auth", authRouter)
 
 app.listen(PORT, () => {
   connectToDB()
